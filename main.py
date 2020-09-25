0f09ad060f8d9722303fcfb83d903f052595662e
Prosammer <spamhinting@gmail.com>
2020-09-25 15:22:05 +0000

    First Commit

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-import pandas as pd    
+import pandas as pd
 from pytrends.request import TrendReq
 
+# Only needs to run once - all requests use this session
 pytrends = TrendReq(hl='en-US', tz=360)
 
-kw_list=["BlockChain"]
-pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='CA', gprop='')
+# Keyword List Below
+kw_list=["Juul","LED Strips", "TikTok"]
+
+# Payload only needed for interest_over_time(), interest_by_region() & related_queries()
+#pytrends.build_payload(kw_list, cat='362', timeframe='today 3-m', geo='CA', gprop='')
 
 
-df = pytrends.interest_by_region()
-df.head(10)
+
+
+df = pytrends.get_historical_interest(kw_list, year_start=2018, month_start=1, day_start=1, hour_start=0, year_end=2020, month_end=8, day_end=1, hour_end=0, cat=0, geo='', gprop='', sleep=0)
+
 print(df)
-
-print("Fuck me it's been a while")
+df.to_pickle(JuulLEDTikTok.pkl) 
