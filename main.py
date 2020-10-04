642a74ccaf1be5204a1d51db0773600df94e5bb1
Prosammer <spamhinting@gmail.com>
2020-10-04 17:57:02 +0000

    Switched to interest_over_time. Also switched back to main pytrends.

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
 import pandas as pd
 import os, requests, time
 from pytrends.request import TrendReq
-#pd.options.plotting.backend = "plotly"
 import plotly.express as px
 
 
-
 # Only needs to run once - all requests use this session
-pytrends = TrendReq(hl='en-US', tz=-240)
+# Timezone is 240 (could be -240 as well?)
+pytrends = TrendReq(hl='en-US', tz=-240,retries=2)
 
-# Keyword List Below
-
-kw_list = ["Juul"]
+kw_list = ["Juul", "Tiktok"]
 
 # Note: Payload only needed for interest_over_time(), interest_by_region() & related_queries()
-# pytrends.build_payload(kw_list =["Juul"], cat='', timeframe='today 3-m', geo='', gprop='')
-try:
-    time.sleep(.3)
-    #df = pytrends.top_charts(2019, hl='en-US', tz=300, geo='GLOBAL')
+# Max number of queries is 5
+pytrends.build_payload(kw_list, timeframe='today 1-m', geo='CA', gprop='')
 
-    # Simple test dataframe:
-        #df = pd.DataFrame(dict(a=[1,3,2], b=[3,2,1]))
-    #   Main data-retrieval function:
-    #df = pytrends.get_historical_interest(kw_list, year_start=2018, month_start=1, day_start=1, hour_start=0, year_end=2019, month_end=2, day_end=1, hour_end=0, cat=0, geo='', gprop='', sleep=15, frequency='daily')
-    #df.to_pickle("Juul.pkl") 
-except requests.exceptions.Timeout:
-    print("Timeout bruv")
+def related_words():
+    # Related Queries, returns a dictionary of dataframes
+    related_queries_dict = pytrends.related_queries()
+    print("Related Queries:\n\n",related_queries_dict)
+
+def suggested_words():
+    suggestions_dict = pytrends.suggestions(keyword='Juul')
+    print("Suggestions:\n\n", suggestions_dict)
+    
+
+def find_interest():
+    try:
+        
+        # Simple test dataframe:
+            #df = pd.DataFrame(dict(a=[1,3,2], b=[3,2,1]))
+        #   Main data-retrieval function:
+        print("Commencing interest_over_time search...")
+        interest_over_time_df = pytrends.interest_over_time()
+        print(interest_over_time_df.head())
+
+        print("Interest_over_time data acquired, pickling...")
+        df.to_pickle(F"{kw_list[0]}_vs_{kw_list[1]}.pkl") 
+    except pytrends.requests.exceptions.Timeout:
+        print("Timeout bruv")
 
 
-
-
+if __name__ =='__main__':
+    print("The name is main, bitch!")
+    related_words()
--- visualizer.py
+++ visualizer.py
@@ -1,10 +1,15 @@
+from main import kw_list
 import pandas as pd
 import os, requests, time
 import plotly.express as px
 
-df = pd.read_pickle("Juul.pkl")
-print(df.head(10))
 
-fig = px.line(df, x="Juul", y="date", title=f"Popularity of the keyword: {kw_list[0]} ")
+df = pd.read_pickle(F"{kw_list[0]}_vs_{kw_list[1]}.pkl")
+del df['isPartial']
+
+#print(df.head(10))
+
+#print(df.index)
+fig = px.line(df, x=df.index, y=["Tiktok","Juul"], title=f"Popularity of the keyword: {kw_list[0]} vs {kw_list[1]} ")
 
 fig.write_html("test.html")
