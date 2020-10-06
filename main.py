2c9ace69fb40465525f77adacfd405068420c017
Prosammer <spamhinting@gmail.com>
2020-10-06 01:03:48 +0000

    Created a masterkeyworldDF and removed all batch jobs (for now)

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import plotly.express as px
 
 
 
-## TODO: Need to find fix so I can use related_topics instead of related_queries
+## TODO: Need to find fix so that I can find 5 related terms at a time (fewer requests)
 
 
 # Only needs to run once - all requests use this session
 # Timezone is 240 (could be -240 as well?)
-pytrends = TrendReq(hl='en-US', tz=-240,retries=2)
+pytrends = TrendReq(hl='en-US', tz=-240,retries=2,backoff_factor=0.2,)
 
 
 # Related Topics, returns a dictionary of dataframes
@@ -19,14 +19,16 @@ def related_topics(current_KW):
     # Max number of queries is 5
     pytrends.build_payload(kw_list=current_KW, timeframe='today 3-m', geo='CA')
     related_topics_dict = pytrends.related_topics()
-
-    for key,innerdict in related_topics_dict.items():
-        for k,df in innerdict.items():
-            # Selects the rising topics of the dataframe (instead of the top ones)
-            if str(k) == 'rising' and df is not None:
-                df = df.drop(columns=['link','value'])
-                df.to_html('temp.html')
-                return(df)
+    try:
+        for key,innerdict in related_topics_dict.items():
+            for k,df in innerdict.items():
+                # Selects the rising topics of the dataframe (instead of the top ones)
+                if str(k) == 'rising' and df is not None:
+                    df = df.drop(columns=['link','value'])
+                    df.to_html('temp.html')
+                    return(df)
+    except:
+        print("Keyword didn't work!")
 
 
 def find_interest():
@@ -41,45 +43,33 @@ def find_interest():
 
 
 
-kw_list = ["Juul","Tiktok","Canada"]
-current_KW_index = 0
+kw_list = ["Canada","Juul","Tiktok","Apple","Buddhism","Axe-Throwing"]
+index_kw_list = 0
 related_topics_df_list = []
 
 
 if __name__ =='__main__':
-    print("The name is main, bitch!")
     
     # -- While loop for assembling keywords -- #
-    #while len(kw_list) <= 10:
+    while len(kw_list) <= 25:
 
-    #Select the first keyword as a single-item list
-    current_KW = [kw_list[current_KW_index]]
-    #Creates a list of dataframes with the returned results
-    related_topics_df_list.append(related_topics(current_KW))
-    
-    current_KW_index +=1
-
-
-
-    # print("Finding similar topics for: \n\n", current_KW)
-    #     #Finds and returns keywords for the current group
-    # latest_new_keywords = related_topics(current_KW)
-    # for i,k in latest_new_keywords:
-    #     for eachdataframe in k:
-    #         print(str(eachdataframe))
-    #     # for i in latest_new_keywords:
-    #     #     if i not in kw_list:
-    #     #         kw_list.append(i)
-    
+        #Select the first keyword as a single-item list
+        current_KW = [kw_list[index_kw_list]]
+        #Creates a list of dataframes with the returned results (to be merged later)
+        newtopics_df = related_topics(current_KW)
+        related_topics_df_list.append(newtopics_df)
+        #Adds keywords to kw_list for efficiency
+        try:
+            for i in newtopics_df.topic_title:
+                kw_list.append(i)
+        except:
+            pass
+        index_kw_list +=1
+        print("Index: ",index_kw_list)
+        print("Length of kw_list",len(kw_list))
+        time.sleep(1)
 
 
-
-
-    # for each5keywords in master_kw_list:
-    #     related_topics(each5keywords)
-    #     # Add related topics to kw_list
-    #     i += 5
-    # keyword_count = 125
-
-    # for each5keywords in master_kw_list:
-    #     find_interest()
+    masterkeywordDF = pd.concat(related_topics_df_list)
+    masterkeywordDF.to_pickle("masterkeywordDF.pkl")
+    masterkeywordDF.to_html('masterkeywordDF.html')
