908ce49a18582a40036dff3e1b41a8f3adcd7bcc
Prosammer <spamhinting@gmail.com>
2020-10-16 04:19:38 +0000

    Moved trendreq into location selector - proxies only used when local

--- interest.py
+++ interest.py
@@ -16,29 +16,23 @@ numofcycles = 10
 
 #BlazingSEO proxy list - uses port 4444 for username/password based auth, and 3128 for IP based auth.
 
+
 proxylist = ['https://104.168.51.141:3128','https://192.186.134.157:3128','https://192.3.214.40:3128','https://104.144.220.10:3128','https://104.144.28.167:3128','https://172.245.181.226:3128','https://192.210.185.193:3128','https://23.236.232.162:3128','https://23.254.68.49:3128','https://23.94.176.161:3128','https://69.4.90.17:3128','https://107.172.94.128:3128','https://138.128.84.159:3128','https://192.186.161.228:3128','https://192.241.64.90:3128','https://192.241.80.170:3128','https://198.12.80.148:3128','https://198.23.217.80:3128','https://23.250.94.234:3128','https://45.72.0.245:3128']
 
 
-# Only needs to run once - all requests use this session
-# Timezone is 240 (could be -240 as well?)
 
-pytrends = TrendReq(hl='en-US', tz=-240,retries=2,backoff_factor=0.2,proxies=proxylist)
-
-
-
-# Connect to the database
-
-
-# Sets which address to use based on CLI input
+# Sets which address to use, and if proxies should be used, based on CLI input
 def sql_DB_settings(location="world"):
-    host = ""
+    logging.info(f"Your chosen location is: {location}")
+    global pytrends
     if location == "local":
         host='private-db-mysql-tor1-72034-do-user-8152651-0.b.db.ondigitalocean.com'
-
+        pytrends = TrendReq(hl='en-US', tz=-240,retries=2,backoff_factor=0.2)
     elif location == "remote":
         host='db-mysql-tor1-72034-do-user-8152651-0.b.db.ondigitalocean.com'
+        pytrends = TrendReq(hl='en-US', tz=-240,retries=2,backoff_factor=0.2,proxies=proxylist)
     else:
-        raise Exception("Must specify location='remote' or 'local'." )
+        raise Exception("Must specify location: 'remote' or 'local'." )
 
     connection = pymysql.connect(host=host,
     user='doadmin',
@@ -47,6 +41,7 @@ def sql_DB_settings(location="world"):
     db='trends',
     charset='utf8mb4',
     cursorclass=pymysql.cursors.DictCursor)
+
     return connection
 
 def retrieve_keyword(cursor):
@@ -104,8 +99,8 @@ if __name__ =='__main__':
 
     logging.info("Greetings, Pleb!")
     logging.info(f"Number of Cycles to run: {numofcycles}")
+    
     connection = fire.Fire(sql_DB_settings)
-
     with connection.cursor() as cursor: 
         for i in range(numofcycles):
                 find_interest(cursor)
